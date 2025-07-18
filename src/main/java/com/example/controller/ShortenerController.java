cbed2214b9792a54aaaf47db4e6cfffec8128f3f
gudurukhasim <116193735+gudurukhasim@users.noreply.github.com>
2025-07-19 00:14:07 +0530

    Update ShortenerController.java

--- src/main/java/com/example/controller/ShortenerController.java
+++ src/main/java/com/example/controller/ShortenerController.java
@@ -27,14 +27,14 @@ public class ShortenerController {
     @PostMapping("/api/shorten")
     public ResponseEntity<?> shorten(@Valid @RequestBody UrlRequest request) {
         String code = service.shortenUrl(request.getOriginalUrl());
-        return ResponseEntity.ok("http://localhost:8081/" + code);
+        return ResponseEntity.ok("https://linkshortener-backend.koyeb.app/" + code); //"http://localhost:8081/"
     }
 
     @GetMapping("/{code}")
     public void redirect(@PathVariable String code, HttpServletResponse response) throws IOException {
         String original = service.getOriginalUrl(code);
         if (original == null) {
-            response.sendRedirect("http://localhost:5173/error?type=expired_or_not_found");
+            response.sendRedirect("https://linkshortner-frontend.vercel.app/error?type=expired_or_not_found");//"http://localhost:5173/error?type=expired_or_not_found"
         } else {
             response.sendRedirect(original);
         }
